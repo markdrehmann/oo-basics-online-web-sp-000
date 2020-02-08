fe746db80d26f8f00cb320142de58662de982d06
Mark Drehmann <markdrehmann@gmail.com>
2020-02-08 22:22:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,13 @@ class Shoe
   end
 
   def cobble
+    self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
 end
 
 # learn spec/02_shoe_spec.rb
+nike(nike) = Shoe.new
+nike.condition = "old"
+nike.cobble
